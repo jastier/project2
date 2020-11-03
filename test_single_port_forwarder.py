9cab6d9a4690a54cb1a69b6e68f457aca21a080f
Joseph Astier <jastier@email.arizona.edu>
2020-11-03 16:16:29 -0700

    Added a test script for the single port forwarder This file will probably evolve into the multi port forwarder

---
+++ test_single_port_forwarder.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+# Debug tester for Joseph's single port forwarding python script.
+
+import os
+
+# used for reporting results
+passStr = ' OK '
+failStr = 'FAIL'
+
+# Perform the test with a system call.  A zero return value is a pass.
+def doTest(t):
+    print('TEST:  '+t)
+    print('OUTPUT:')
+    retval = (failStr, passStr)[(os.system(t)== 0)]
+    print('RESULT: ' + retval + '\n')
+    return (retval, t)
+  
+
+# Tests to perform.  They should probably have names
+tests = (
+    './exitOK.py',
+    './single_port_forwarder.py -d localhost -p 80',
+    './single_port_forwarder.py -d localhost -p 80 --start',
+    './single_port_forwarder.py -d localhost -p 80 --status',
+    './single_port_forwarder.py -d localhost -p 80 --stop',
+    './single_port_forwarder.py -d localhost -p 80 --start --status --stop',
+    './exitFAIL.py',
+    './single_port_forwarder.py',
+    './single_port_forwarder.py -d localhost',
+    './single_port_forwarder.py -p blah',
+    './single_port_forwarder.py -p 80',
+)
+
+# run the tests.  This output can be rather verbose.
+results = map(doTest, tests)
+tested = len(results)
+passed = sum(map(lambda (a,b): (0,1)[a==passStr], results))
+failed = tested-passed
+
+# report the results in a concise way
+print('Testing Summary: ')
+for g in results:
+    print(g[0] + ' ' + g[1])
+print('Tested: ' + str(tested))
+print('Passed: ' + str(passed))
+print('Failed: ' + str(failed))
+if(failed == 0):
+    print('All tests passed.')
+
