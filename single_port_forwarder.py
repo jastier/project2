5b2b7a7aad198f7a2df49df915b45f10d0c24e55
Joseph Astier <jastier@email.arizona.edu>
2020-11-03 18:26:40 -0700

    Cleaned up the comments, added boilerplate header

--- single_port_forwarder.py
+++ single_port_forwarder.py
@@ -1,46 +1,64 @@
 #!/usr/bin/env python
+#
+# Author: Joseph Astier
+# Date: 2020 October 
+#
+# Single action port forwarder.  This script will manage forwarding
+# one port from this machine to a destination machine, by starting an
+# ssh process that will maintain the port forwarding in the background.
+#
+# Required arguments:
+#
+# -d         Destination, either a hostname or IPV4/IPV6 address
+#
+# -p         Port to be forwarded to the destination machine
+#
+#
+# Optional actions (user chooses only 1, any others are ignred):
+#
+# --start    Begin forwarding the port by starting a background ssh process
+#
+# --stop     Stop the port by killing the ssh process.  
+#
+# --status   Report whether the port is currently in use.
+#
 
 import argparse
 import os
 
 parser = argparse.ArgumentParser()
 
-# Destination, this can be a hostname or an IPV4/IPV6 Internet address
+# add args
 parser.add_argument(
     '-d', type=str, required=True, 
-    help='Destination name or IP address'
+    help='Destination hostname or IP address'
 )
-
-# Port to be forwarded
 parser.add_argument(
     '-p', type=int, required=True, help='Port number to forward'
 )
 
-# Start forwarding this port
+# add actions
 parser.add_argument(
     '--start', action='store_true', help='Start forwarding this port'
 )
-
-# Stop forwarding this port
 parser.add_argument(
     '--stop', action='store_true', help='Stop forwarding this port'
 )
-
-# Query the status of this port
 parser.add_argument(
     '--status', action='store_true', help='Query the port status'
 )
 
 args = parser.parse_args()
+print(args)
 
-# Forward the port
+
+# Forward the port using ssh to manage it in the background
 if(args.start):
 
-    preamble = 'ssh -o ConnectTimeout=7 -NfL '
+    p = str(args.p)
+    cmd = 'ssh -o ConnectTimeout=7 -NfL ' + p + ':localhost:' + p + ' '+args.d
   
     try:
-        port = str(args.p)
-        cmd = preamble + port + ':localhost:' + port + ' ' + args.d
         print('Executing command')
         print(cmd)
         os.system(cmd)
@@ -51,14 +69,16 @@ if(args.start):
 
     except: 
         print('some other exception')
-
-# Query the status of the port
-if(args.status):
-    print('Status')
   
-
-
-# Unbind the port...maybe
-if(args.stop):
+# Stop forwarding the port by killing the ssh process managing this 
+# port, if one exists.  
+else if(args.stop):
      print('Stop')
+
+# Query the status of the port by checking if there is an ssh process
+# with destination and port number already running.  
+# TODO:  Detect if a port has been bound by any process, not just ours.
+else if(args.status):
+    print('Status')
+
 
