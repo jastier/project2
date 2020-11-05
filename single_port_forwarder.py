fd1348d3563da88268554f5050b1dc4687d293c0
Joseph Astier <jastier@email.arizona.edu>
2020-11-05 16:17:40 -0700

    Cleaned up the ssh background processes discovery logic

--- single_port_forwarder.py
+++ single_port_forwarder.py
@@ -3,11 +3,11 @@
 # Author: Joseph Astier
 # Date: 2020 October 
 #
-# Single-action port forwarder.  This script will manage forwarding
-# one port from this machine to a destination machine, by starting an
-# ssh process that will maintain the port forwarding in the background.
+# Single Port Forwarder.  This script will manage forwarding one 
+# port from this machine to a destination machine, by starting an ssh
+# process that will maintain the port forwarding in the background.
 # The command used to start the forwarding is later used to check for a
-# running process, or to stop it.
+# running process.
 #
 # Required arguments:
 #
@@ -34,7 +34,7 @@ parser = argparse.ArgumentParser()
 # add args
 parser.add_argument(
     '-d', type=str, required=True, 
-    help='Destination hostname or IP address'
+    help='Host, either a hostname or IP address'
 )
 parser.add_argument(
     '-p', type=int, required=True, help='Port number to forward'
@@ -57,15 +57,17 @@ ssh_cmd = 'ssh -o ConnectTimeout=7 -NfL ' + port + ':localhost:' + port + ' ' + 
 # Find the pid(s) for any processes running our ssh command.  If any exist,
 # the port has already been forwarded.
 pids = []
-ps_cmd = 'ps -C \'' + ssh_cmd + '\' -o pid='
-output = os.popen(ps_cmd).read()
-outputLines = output.splitlines() # ps returns pids one per line
-for line in outputLines:  
-    try:
-        pids.append(int(line))
-    except:
-        continue
-    
+pid_cmd = 'ps -C ssh'
+for line in os.popen(pid_cmd).read().splitlines():
+    tokens = line.split()
+    if(len(tokens) > 0):
+        token = tokens[0]
+        if(token.isdigit()):
+            args_cmd = 'ps -p ' + token + ' -o args='
+            args_output = os.popen(args_cmd).read().rstrip('\n')
+            if(ssh_cmd == args_output):
+                pids.append(token)
+
 
 # Forward the port by starting a process with the ssh command
 if(args.action == START):
@@ -78,8 +80,7 @@ if(args.action == START):
             print('Could not forward port ' + port + ' to ' + host)
     else:
         for pid in pids:
-            print('Port '+port+' was already forwarded to '+host+' ['+str(pid)+']')
-        print('Nothing to do.')
+            print('Port '+port+' was already forwarded to '+host+' ['+pid+']')
   
 
 # Stop forwarding the port(s) by killing their pids
@@ -89,18 +90,17 @@ elif(args.action == STOP):
         cmd = 'kill ' + str(pid)
         try:
             os.system(cmd)
-            print('Port '+port+' is no longer forwarded to '+host +' ['+str(pid)+']')
+            print('Port '+port+' is no longer forwarded to '+host +' ['+pid+']')
         except: 
             print('Could not unforward port ' + port + ' from ' + host)
     if(len(pids) == 0):
         print('Port '+port+' was not forwarded to '+host)
-        print('Nothing to do.')
 
 
 # Query the status of the port by testing if it already has an ssh process
 else:  # STATUS
     for pid in pids:
-        print('Port '+port+' is forwarded to '+host+' ['+str(pid)+']')
+        print('Port '+port+' is forwarded to '+host+' ['+pid+']')
     if(len(pids) == 0):
         print('Port ' + port + ' is not forwarded to ' + host)
         
